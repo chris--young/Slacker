6c35ca2a4236ac244b26b4f5bcf17b622a61e84e
Chris Young <cyoung@mobiquityinc.com>
2014-07-13 15:28:08 -0400

    Tweaked the list action ouput formatting

--- actions/list.js
+++ actions/list.js
@@ -8,7 +8,7 @@ var action = {
   execute: function(data) {
     var output = '*Available Actions*\n'
     for (var x = 0; x < bot.actions.length; x++)
-      output += '- *' + bot.actions[x].trigger + '*: ' + bot.actions[x].description + '\n'
+      output += '`' + bot.actions[x].trigger + '` ' + bot.actions[x].description + '\n'
     return output
   }
 }

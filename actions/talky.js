bf6f44fc49b09322ee80a1dfc3d4b26b72b5fa67
Ustice <ustice@gmail.com>
2014-07-16 10:53:58 -0400

    Adds Talky video conferencing action.

---
+++ actions/talky.js
@@ -0,0 +1,25 @@
+var bot = require(__dirname + '/../bot.js');
+
+var id = require('../library/id.js')();
+
+var action = {
+  name: 'talky',
+
+  trigger: /^talky\s*(.*)$/,
+
+  description: 'Create a talky room for video conferencing and video chatting.',
+
+  execute: function(data, callback) {
+    var payload = {
+      username: 'Talky',
+      icon_emoji: ':app-talky:',
+      text: 'Join the conference: <http://talky.io/' + id + '>.'
+    };
+
+    data = data || id;
+    
+    callback(payload.text);
+  }
+};
+
+bot.addAction(action);

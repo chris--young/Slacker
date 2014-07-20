4e8744e58c13c22d036807189ba18a7a9e135569
Ustice <ustice@gmail.com>
2014-07-20 05:00:53 -0400

    Normalizes command array

--- library/parse.js
+++ library/parse.js
@@ -1,5 +1,8 @@
 'use strict';
 
+var _ = require('lodash');
+var uuid = require(__dirname  + '/id.js');
+
 var delimiters = [' ', '|', '>', ';'];
 
 var STATES = ['default', 'quotes', 'doubleQuotes', 'escape'];
@@ -64,20 +67,20 @@ exports.slackText = function(encodedText) {
  * @return {Array}        Array of tokens, including commands, arguments, switches, resources, etc.
  */
 function commands (tokens, input) {
-  var token = '';
+  var commandId = '';
   if (typeof tokens === 'string') {
     input = tokens;
-    tokens = [{type: 'command', value: ''}];
+    tokens = [];
   }
 
   input = exports.slackText(input);
 
   var chars = input.split('');
 
-  var state = 'command';
+  var state = 'operator';
   var quotes = '';
   var index = 0;
-  var boundary = false;
+  var boundary = true;
   while (index < input.length) {
     switch (chars[index]) {
       case '\'':
@@ -129,14 +132,17 @@ function commands (tokens, input) {
       default:
         if (boundary && state === 'command' || state === 'switch') state = 'argument';
         if (boundary && state === 'operator') {
-          if (tokens[tokens.length - 1].value === '>') state = 'resource';
+          if (tokens.length === 0) state = 'command';
+          else if(tokens[tokens.length - 1].value === '>') state = 'resource';
           else state = 'command';
         } 
         break;
     }
 
+
     if (boundary && !chars[index].match(/[ '"]/)) {
-      tokens.push({type: state, value: (chars[index] !== ' ') ? chars[index] : ''});
+      commandId = (state !== 'command') ? commandId : uuid();
+      tokens.push({type: state, value: (chars[index] !== ' ') ? chars[index] : '', id: commandId});
       boundary = (state === 'operator');
     } else if (!chars[index].match(/[ '"]/)) {
       tokens[tokens.length - 1].value += chars[index];
@@ -145,7 +151,63 @@ function commands (tokens, input) {
 
     index++;
   }
-  return tokens;
+  return normalize(tokens);
+}
+
+function normalize (commands) {
+  var normalizedCommands = [];
+
+  _.each(commands, function (token) {
+    var resourceTypes = {
+      '@': 'user',
+      '#': 'channel',
+      '%': 'group',
+      '/': 'file'
+    };
+
+    var thisCommand = _.find(normalizedCommands, {id: token.id});
+    switch (token.type) {
+      case 'command':
+        token.arguments = [];
+        token.switches = [];
+        token.pipe = false;
+        token.redirectTo = [];
+        normalizedCommands.push(token);
+        thisCommand = _.find(normalizedCommands, {id: token.id});
+        break;
+
+      case 'argument':
+        thisCommand.arguments.push(token.value);
+        break;
+
+      case 'switch':
+        if (token.value.indexOf('--')) {
+          thisCommand.switches.push(token.value.replace(/^-*/, ''));
+        } else {
+          _.each(token.value.replace(/^-/, '').split(), function (switchChar){
+            thisCommand.switches.push(switchChar);
+          });
+        }
+        break;
+
+      case 'operator':
+        if (token.value === '|') thisCommand.pipe = true;
+        break;
+
+      case 'resource':
+        if (resourceTypes[token.value.substring(0,1)])
+          thisCommand.redirectTo.push({type: resourceTypes[token.value.substring(0,1)] , name: token.value.substring(1)});
+        else 
+          thisCommand.redirectTo.push({type: 'file' , name: token.value});
+        break;
+
+      default:
+        throw new Error('Parse error. ' + JSON.stringify(token));
+        break;
+    }
+  });
+
+  return normalizedCommands;
 }
 
 /** 

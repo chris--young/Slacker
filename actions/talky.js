0c41c847e53bf372235523fd0afc105706eb93b4
Ustice <ustice@gmail.com>
2014-12-17 23:44:41 -0500

    Improves help formatting.

--- actions/talky.js
+++ actions/talky.js
@@ -8,10 +8,10 @@ var action = {
   description: 'Create a talky room for video conferencing and video chatting.',
 
   helpText: '' +
-  'Create a talky room for video conferencing and video chatting.\n\n' +
-
-    '`/slacker talky [room_name]`\n\n' +
-
+    'Create a talky room for video conferencing and video chatting.\n' +
+    '```' +
+    '/slacker talky [room_name]' +
+    '```\n' +
     'If the optional `room_name` is inlcuded, then it will be used in the form of `http://talky.io/room_name`. If not a random one will be chosen.'
   ,
 
